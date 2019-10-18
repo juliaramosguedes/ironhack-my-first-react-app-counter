73383b9975024acd012b5135d8b96d71fa31f001
Henrique Guazzelli Mendes <henriqueguazzellimendes@Henriques-MBP.localdomain>
2019-10-17 21:04:52 -0300

    first app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Counter from './components/Counter/Counter';
+import Button from './components/Button/Button';
+import Text from './components/Text/Text';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counter: 0,
+    counterTrigger: false,
+  };
+
+  addCounter = () => {
+    const { counter } = this.state;
+
+    if (counter < 10) {
+      this.setState({ counter: counter + 1 }, () => {
+        if (this.state.counter === 10) this.setState({ counterTrigger: true })
+      });
+    }
+
+  };
+
+  render() {
+    const { counter, counterTrigger } = this.state;
+
+    return (
+      <div className="app-container">
+        <Counter text="Our counter is at " counter={counter}/>
+        <Button classStyles="is-success" onClickFunction={this.addCounter}>Add Counter</Button>
+        {counterTrigger
+          ? <Text>You reached 10 in your counter. Sorry, you can't add more :-(</Text>
+          : <Text>Click the button to add 1 to our counter :-)</Text>
+        }
+      </div>
+    );
+  };
 }
 
 export default App;
